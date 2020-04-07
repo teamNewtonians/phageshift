01e5bbcc86e57eaba2dbb9cc84a0b7be9f7738d1
dtandre <dtandre@g.clemson.edu>
2020-04-07 16:58:39 -0400

    added my WIP

---
+++ Team-waveShooter/Assets/derek/Phage/mouseRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseRotate : MonoBehaviour {
+    public GameObject projectile;
+    public GameObject shootFrom;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if (plane.Raycast(ray, out distance)) {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            GameObject bullet = Instantiate(projectile, shootFrom.transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 100,ForceMode.Impulse);
+        }
+
+    }
+}
