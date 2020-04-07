3d5a8b90785dd7b8d70c68c5867cf85832009d62
Benjimoro <benbwarner@gmail.com>
2020-04-07 11:26:55 -0400

    Added veiny skybox

--- Team-waveShooter/Assets/Scripts/fieldGenerator.cs
+++ Team-waveShooter/Assets/Scripts/fieldGenerator.cs
@@ -31,40 +31,7 @@ public class fieldGenerator : MonoBehaviour
         bloodstream = GameObject.Find("bloodstream").GetComponent<ParticleSystem>();
         bloodstream.Stop();
         doorAnim.SetBool("doorOpen", doorOpen);
-        for (int x = -size/2; x <= size/2; x += grid)
-        {
-            for (int z = -size/4; z <= size/4; z += grid)
-            {
-                rndWall = Random.Range(0, 2);
-                rndTree = Random.Range(0, 3);
-                rndYofs = Random.Range(0, 3);
-                if(rndWall == 1)
-                {
-                    clonePos = new Vector3( x, rndYofs, z);
-                    walls.Add(Instantiate(cellWallZ, clonePos, cellWallZ.transform.rotation));
-                }
-                if (rndTree == 1)
-                {
-                    clonePos = new Vector3(x, rndYofs-1, z+4);
-                    trees.Add(Instantiate(optTree, clonePos, optTree.transform.rotation));
-                }
-            }
-            
-        }
-        for (int z = -size/2; z <= size/2; z += grid)
-        {
-            for (int x = -size/4; x <= size/4; x += grid)
-            {
-                rndWall = Random.Range(0, 2);
-                rndYofs = Random.Range(0, 3);
-                if (rndWall == 1)
-                {
-                    clonePos = new Vector3(x, rndYofs, z);
-                    walls.Add(Instantiate(cellWallX, clonePos, cellWallX.transform.rotation));
-                }
-            }
-
-        }
+        fieldMaker();
     }
 
     // Update is called once per frame
@@ -81,10 +48,58 @@ public class fieldGenerator : MonoBehaviour
         }
         if ((int)counter == 0 && doorOpen)
         {
+            fieldMaker();
             doorOpen = false;
             doorAnim.SetBool("doorOpen", doorOpen);
             bloodstream.Stop();
             counter = 10.0f;
+        }
+    }
+    void fieldMaker()
+    {   
+        for(int i = 0; i< walls.Count; i++)
+        {
+            Destroy(walls[i]);
+        }
+        for (int i = 0; i < trees.Count; i++)
+        {
+            Destroy(trees[i]);
+        }
+        walls = new List<GameObject>(0);
+        trees = new List<GameObject>(0);
+        for (int x = -size / 2; x <= size / 2; x += grid)
+        {
+            for (int z = -size / 4; z <= size / 4; z += grid)
+            {
+                rndWall = Random.Range(0, 2);
+                rndTree = Random.Range(0, 3);
+                rndYofs = Random.Range(0, 3);
+                if (rndWall == 1)
+                {
+                    clonePos = new Vector3(x, rndYofs, z);
+                    walls.Add(Instantiate(cellWallZ, clonePos, cellWallZ.transform.rotation));
+                }
+                if (rndTree == 1)
+                {
+                    clonePos = new Vector3(x, rndYofs - 1, z + 4);
+                    trees.Add(Instantiate(optTree, clonePos, optTree.transform.rotation));
+                }
+            }
+
+        }
+        for (int z = -size / 2; z <= size / 2; z += grid)
+        {
+            for (int x = -size / 4; x <= size / 4; x += grid)
+            {
+                rndWall = Random.Range(0, 2);
+                rndYofs = Random.Range(0, 3);
+                if (rndWall == 1)
+                {
+                    clonePos = new Vector3(x, rndYofs, z);
+                    walls.Add(Instantiate(cellWallX, clonePos, cellWallX.transform.rotation));
+                }
+            }
+
         }
     }
 }
