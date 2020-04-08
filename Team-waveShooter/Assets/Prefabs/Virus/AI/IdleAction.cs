956bb4c8bd112b32ea710b26b94580bbd64cf9e2
Hayden Lewis <whlewis@g.clemson.edu>
2020-04-08 16:02:10 -0400

    Virus AI
    Complete virus AI, NavMesh generated for map, dynamically changes with wall generation.
    NavMeshAgent added to phageNew

---
+++ Team-waveShooter/Assets/Prefabs/Virus/AI/IdleAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Actions/Idle")]
+
+public class IdleAction : Action
+{
+  public override void Act(StateController controller)
+  {
+    Idle(controller);
+  }
+
+  private void Idle(StateController controller)
+  {
+
+  }
+}
---
+++ Team-waveShooter/Assets/Prefabs/Virus/AI/VirusManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusManager : MonoBehaviour
+{
+
+  public int enemiesAlive;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  public int enemiesRemaining()
+  {
+    return enemiesAlive;
+  }
+}
