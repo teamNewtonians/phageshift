2b5f5b71733831d78e81a32221a11b8d479b91cd
Hayden Lewis <whlewis@g.clemson.edu>
2020-04-08 18:37:41 -0400

    Virus Chase Implemented
    Virus chase function implemented, old state machine design removed, functionality moved to VirusControl

---
+++ Team-waveShooter/Assets/Prefabs/Virus/AI/VirusControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VirusControl : MonoBehaviour
+{
+  public Transform chaseTarget;
+  public NavMeshAgent navMeshAgent;
+
+  void Start()
+  {
+    chaseTarget = GameObject.FindWithTag("Player").transform;
+  }
+
+  void Update()
+  {
+    navMeshAgent.destination = chaseTarget.position;
+    navMeshAgent.Resume();
+  }
+}
