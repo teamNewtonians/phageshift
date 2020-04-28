004157dd062e8ce28995c95bc79d301065b5ae2c
Benjimoro <benbwarner@gmail.com>
2020-04-27 20:17:38 -0400

    volume controls!!!

--- Team-waveShooter/Assets/Prefabs/Phage/cameraScript.cs
+++ Team-waveShooter/Assets/Prefabs/Phage/cameraScript.cs
@@ -6,6 +6,7 @@ public class cameraScript : MonoBehaviour
 {
     public GameObject player;        //Public variable to store a reference to the player game object
 
+    public AudioSource auSrc;
 
     private Vector3 offset;            //Private variable to store the offset distance between the player and camera
 
@@ -24,6 +25,15 @@ public class cameraScript : MonoBehaviour
     void LateUpdate() {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.transform.position + offset;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            auSrc.volume += 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            auSrc.volume -= 0.1f;
+        }
 
         if (Input.GetKey(KeyCode.Q)) //Rotates right when Q is pressed
         {
